5d427c5ae2872277832da1da85a2863f43fa17e1
Aidan Dodds <aidan.dodds@googlemail.com>
2022-08-19 23:31:22 +0100

    Add pascal triangle test

---
+++ tests/pascal.c
@@ -0,0 +1,51 @@
+
+int number(int v) {
+    int x;
+    x = v % 10;
+    if (v) {
+      number(v / 10);
+      putchar('0' + x);
+    }
+}
+
+int binomialCoeff(int n, int k)
+{
+    int res, i;
+    res = 1;
+    if (k > n - k)
+        k = n - k;
+    for (i = 0; i < k; i=i+1)
+    {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+      
+    return res;
+}
+
+void printPascal(int n)
+{
+    int line, i;
+    // Iterate through every line and
+    // print entries in it
+    for (line = 0; line < n; line=line+1)
+    {
+        // Every line has number of 
+        // integers equal to line 
+        // number
+        for (i = 0; i <= line; i=i+1) {
+            number(binomialCoeff(line, i));
+            putchar(' ');
+        }
+        putchar(10);
+    }
+}
+
+// Driver program 
+int main()
+{
+    int n;
+    n = 7;
+    printPascal(n);
+    return 0;
+}
